3c41fa4d0e8a149089be547a54103ba91db9ee21
Jamie Cummings <jamiecummings@macbook-pro-2.local>
2018-08-30 09:28:53 -0500

    Initial Commit

---
+++ Video Game Library/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Video Game Library
+//
+//  Created by Jamie Cummings on 8/30/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
